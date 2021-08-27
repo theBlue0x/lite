da1722d0502bb3537b833831bdf358b2d205ca93
theBlue0x <3836882+theBlue0x@users.noreply.github.com>
2021-08-27 16:15:35 -0400

    qr code scanner

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,25 @@
 // Get the modal
-var modal = document.getElementById("qrReader");
+var modal = document.getElementById("qrreader");
 
 // Get the button that opens the modal
-var btn = document.getElementById("qrModal");
+var btn = document.getElementById("qr");
 
-var close = document.getElementById("qrClose");
+// Get the <span> element that closes the modal
+var span = document.getElementById("qrclose");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "inline-block";
+  modal.style.display = "block";
 }
 
-// When the user clicks close button, close the modal
-close.onclick = function() {
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
   modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
