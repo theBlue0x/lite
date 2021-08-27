8a01aab9d2297ceb5f5d0feb411e090fb4641b74
theBlue0x <3836882+theBlue0x@users.noreply.github.com>
2021-08-27 17:08:24 -0400

    camera

--- js/modal.js
+++ js/modal.js
@@ -4,7 +4,7 @@ var modal = document.getElementById("qrreader");
 // Get the button that opens the modal
 var btn = document.getElementById("qr");
 
-// Get the <span> element that closes the modal
+// Get the element that closes the modal
 var span = document.getElementById("qrclose");
 
 // When the user clicks on the button, open the modal
@@ -12,14 +12,7 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on close, close the modal
 span.onclick = function() {
   modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
