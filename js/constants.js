7ef4084d27257743fe08b988494406ee720dd154
theBlue0x <3836882+theBlue0x@users.noreply.github.com>
2021-10-19 15:00:18 -0400

    Initial

--- js/constants.js
+++ js/constants.js
@@ -1,25 +1,12 @@
 var Constants = (function(Constants, $, undefined) {
-
   Constants.isTestnet = false;
-
-  var mainnetUSDXId = '9756711664096254982';
-  var testnetUSDXId = '9429933812817993614';
+  var mainnetUSDXId = '1428996829038862536';
+  var testnetUSDXId = '';
   Constants.USDXId = mainnetUSDXId;
-
   if(Constants.isTestnet)
   {
-    Constants.USDXId = testnetUSDXAssetId;
+    Constants.USDXId = testnetUSDXId;
   }
-
-  Constants.nxtpassApiUrl = 'http://127.0.0.1:17776/nxtpass';
-
-  // 2 weeks default listing duration
-  Constants.listingDurationDefault = 2 * 7 * 24 * 60 * 60;
-  // 4 weeks extended listing duration
-  Constants.listingDurationExtended = 4 * 7 * 24 * 60 * 60;
-  // listing fees (for information only)
-  Constants.listingFeeDefault = 0;
-  Constants.listingFeeExtended = 0;
-
+  Constants.apiUrl = 'https://wallet.blue0x.com/nxt';
   return Constants;
 } (Constants || {}, jQuery));
