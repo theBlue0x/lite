6296732e23fd37747cdd0c879c7c2ce2a0202b55
theBlue0x <3836882+theBlue0x@users.noreply.github.com>
2021-12-30 14:34:41 -0500

    add all currencies

--- js/constants.js
+++ js/constants.js
@@ -1,12 +1,31 @@
 var Constants = (function(Constants, $, undefined) {
-  Constants.isTestnet = false;
-  var mainnetUSDXId = '1220759651735613679';
-  var testnetUSDXId = '';
-  Constants.USDXId = mainnetUSDXId;
-  if(Constants.isTestnet)
-  {
-    Constants.USDXId = testnetUSDXId;
-  }
+  
+  Constants.USDXId = '6242187520250422526';
+
+  Constants.AUDXId = '11676984573525209263';
+
+  Constants.CADXId = '2183128855145070353';
+
+  Constants.CHFXId = '6248402454397477058';
+
+  Constants.CNYXId = '10313324264777951073';
+
+  Constants.EURXId = '17316394178219449698';
+
+  Constants.GBPXId = '5110227727445751126';
+
+  Constants.IQDXId = '1443030428487847127';
+
+  Constants.IRRXId = '11876333508008631635';
+
+  Constants.JPYXId = '14731857139263877382';
+
+  Constants.NZDXId = '140656572595178442';
+
+  Constants.RUBXId = '15255615064190511978';
+
   Constants.apiUrl = 'https://wallet.blue0x.com/nxt';
+
   return Constants;
+
 } (Constants || {}, jQuery));
